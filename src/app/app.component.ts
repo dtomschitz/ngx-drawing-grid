eefa63c15d9f2c076c7d496118a228293f542a9b
David Tomschitz <david@tomschitz.de>
2020-09-24 18:16:35 +0200

    feat(test-app): added a test app for showing how to use the drawing grid

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, AfterViewInit, OnInit } from '@angular/core';
+import { DrawingGridService, Pixel } from 'drawing-grid';
+import { PaintingMode } from 'projects/ng-drawing-grid/src/lib/models';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'ng-drawing-grid';
+export class AppComponent implements OnInit {
+  private readonly destroy$: Subject<void> = new Subject<void>();
+
+  width: number;
+  height: number;
+  nodeSize = 28;
+
+  private paintingMode: PaintingMode;
+
+  constructor(private host: ElementRef, private gridService: DrawingGridService) {}
+
+  ngOnInit() {
+    this.gridService.paintingMode$.pipe(takeUntil(this.destroy$)).subscribe((paintingMode) => {
+      this.paintingMode = paintingMode;
+    });
+
+    this.width = this.host.nativeElement.clientWidth;
+    this.height = this.host.nativeElement.clientHeight - 64;
+  }
+
+  onMouseDown({ x, y }: Pixel) {
+    this.fillPixel(x, y);
+  }
+
+  onMouseMove({ x, y }: Pixel) {
+    this.fillPixel(x, y);
+  }
+
+  onMouseUp({ x, y }: Pixel) {}
+
+  onContextMenu({ x, y }: Pixel) {
+    this.gridService.clearPixel(x, y);
+  }
+
+  private fillPixel(x: number, y: number) {
+    if (this.paintingMode === PaintingMode.ERASE) {
+      this.gridService.clearPixel(x, y);
+      return;
+    }
+
+    this.gridService.fillPixel(x, y, 'black');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
+import { DrawingGridModule } from 'drawing-grid';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, BrowserAnimationsModule, DrawingGridModule, MatToolbarModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
